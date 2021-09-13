7a247e058bf3e3227cd640eb933b38188f5e73d4
Ali <muhammadalised@gmail.com>
2021-09-13 19:53:08 +0500

    Added some more data features in Admin

--- listings/models.py
+++ listings/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 from realtors.models import Realtor
 
-# Create your models here.
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
