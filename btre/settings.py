776992c3cd46cc1e4e151b424dbb737ab4d6e640
Ali <muhammadalised@gmail.com>
2021-09-13 15:15:15 +0500

    Added static files setting and added some data

--- btre/settings.py
+++ btre/settings.py
@@ -134,3 +134,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/media/'
