0880722193cafead23b2c7ad0bfbf541466ec7b4
Ali <muhammadalised@gmail.com>
2021-09-14 17:11:22 +0500

    Set search choices in pages/index.html and listings/search.html

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 
 from .models import Listing
+from .choices import bedroom_choices, price_choices, state_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -25,4 +26,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+    }
+    
+    return render(request, 'listings/search.html', context)
