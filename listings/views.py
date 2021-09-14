3b116cc466c86cee8f971968f33babb87fa23465
Ali <muhammadalised@gmail.com>
2021-09-14 14:29:05 +0500

    Added dynamic listing data to listings page

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 
-# Create your views here.
+from .models import Listing
+
 def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
         'page': 'listings',
+        'listings': paged_listings,
     }
+
     return render(request, 'listings/listings.html', context)
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
