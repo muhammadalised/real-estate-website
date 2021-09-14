c8d4e181ca0ed114cadd8d7fc3c6c5e7d968d134
Ali <muhammadalised@gmail.com>
2021-09-14 15:19:27 +0500

    Added dynamic data to index and about pages

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from listings.models import Listing
+from realtors.models import Realtor
+
 def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
     context = {
         'page': 'index',
+        'listings': listings
     }
     return render(request, 'pages/index.html', context)
 
 def about(request):
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
     context = {
-        'page': 'about'
+        'page': 'about',
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
     }
+    
     return render(request, 'pages/about.html', context)
