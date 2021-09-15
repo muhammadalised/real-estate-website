8af347c68722af097cf20cc4da2f26618d0756b1
Ali <muhammadalised@gmail.com>
2021-09-15 16:56:40 +0500

    Added alert messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def register(request):
+    if request.method == "POST":
+        messages.error(request, 'Testing Error Message')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        # Login User
+        return
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
-
-def register(request):
-    return render(request, 'accounts/register.html')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
--- btre/settings.py
+++ btre/settings.py
@@ -140,3 +140,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media Folder Settings
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
